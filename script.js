59dbc05166d86f918b7fd35b549d13fd4dd2c2b7
savi savi <74069795+savi1047@users.noreply.github.com>
2025-06-21 19:31:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const textToType = "Driving Spotify's Growth, One Insight at a Time.";
+    const typingElement = document.querySelector('.typing-effect');
+    let i = 0;
 
+    function typeWriter() {
+        if (i < textToType.length) {
+            typingElement.innerHTML += textToType.charAt(i);
+            i++;
+            setTimeout(typeWriter, 100); // Adjust typing speed here
+        }
+    }
+
+    typeWriter();
+
+    // Smooth scrolling for navigation links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
